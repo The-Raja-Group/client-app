b43812ee5df2f80b43ed926b1d746828df984613
jongawan <jongawan@gmail.com>
2024-02-21 11:22:02 +0700

    feat:added nav bar

--- VidCallApp/src/App.jsx
+++ VidCallApp/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
+import Homepage from './components/Homepage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline"> Hello world! </h1>
+      {/* <h1 className="text-3xl font-bold underline"> Hello world! </h1> */}
+      <Homepage/>
     </>
   )
 }
