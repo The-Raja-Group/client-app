ef065f5e65a2d005dea06b0b2b1d6982b97762a6
“maulanaFandi” <maulana27fandi@gmail.com>
2024-02-21 13:35:03 +0700

    Feat: Context,

--- VidCallApp/src/App.jsx
+++ VidCallApp/src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
+import videoPlayer from './components/videoPlayer'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <videoPlayer />
       <h1 className="text-3xl font-bold underline"> Hello world! </h1>
     </>
   )
